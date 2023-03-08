abc196fe097d86dceb24c1fa26006ae2e8f44fea
andy00614 <33732843@qq.com>
2023-03-08 14:49:33 +0800

    chore: add sass

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import styles from '@/styles/Home.module.scss'
 
 export default function Home() {
   return (
@@ -15,6 +11,11 @@ export default function Home() {
         <link rel="icon" href="/crypto.png" />
       </Head>
       <main className={styles.main}>
+        <div style={{ padding: 100 }}>
+          <section>
+            <span>Address: {}</span>
+          </section>
+        </div>
       </main>
     </>
   )
