110af4eccb75fe5f2800b540ec31ea46f9b1583f
andy00614 <33732843@qq.com>
2023-03-10 16:10:16 +0800

    feat: style optimization

--- components/Wallet.tsx
+++ components/Wallet.tsx
@@ -1,12 +1,13 @@
-import { Button, message, Spin, Tag } from "antd"
+import { Button, message, Spin, Tag, Typography } from "antd"
 import { useMemo, useState } from "react"
 import { walletGenerate, walletAddress, walletPKey } from '@/packages/web3'
 import styles from '@/styles/Wallet.module.scss'
 import { request } from "@/packages/lib/request"
+import Image from "next/image"
 
 const colors = ['#F8B195', '#F67280', '#C06C84', '#6C5B7B', '#355C7D', '#2A363B', '#A2D5C6', '#E9D78E', '#EFEA5A', '#C5BC8E', '#F8B195', '#F67280', '#E9D78E', '#C06C84', '#A2D5C6']
 
-
+const { Paragraph } = Typography;
 
 function getRandomColor() {
   const randomIndex = Math.floor(Math.random() * colors.length);
@@ -53,11 +54,20 @@ const Wallet = (props: {gencallBack: () => void}) => {
       <Button type="primary" onClick={generateWallet} className={styles.btnWallet}>造钱包</Button>
       <Button>记住了</Button>
       <div className={styles.address}>
-        <div>助记词：
+        <div className={styles.word}>
+          助记词：
           {words}
         </div>
-        <span>Address地址: {wallet.public}</span>
-        <span>Private Key秘钥: {wallet.private}</span>
+        <span className={styles.publicKey}>
+          <Image src="/public.png" alt="public" width={24} height={24}/>
+          <b>Address 地址:</b> 
+          {wallet.public && <Paragraph copyable>{wallet.public}</Paragraph>}
+        </span>
+        <span className={styles.privateKey}>
+        <Image src="/private.png" alt="private" width={24} height={24}/>
+          <b>Private Key 秘钥:</b> 
+          {wallet.private && <Paragraph copyable>{wallet.private}</Paragraph>}
+        </span>
       </div>
     </Spin>
   </div>
